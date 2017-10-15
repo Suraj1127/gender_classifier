60175ff55fcee896defe5f0cacd7d958fc73daae
Suraj <regmi125@gmail.com>
2017-10-15 10:40:18 +0545

    input_validation

--- gender_predictor.m
+++ gender_predictor.m
@@ -1,6 +1,13 @@
 load('theta_values');
 name = input("Enter your name:\n", 's');
 name = upper(name);
+name = strtrim(name);
+[temp1 temp2] = regexp(name,'[a-zA-Z ]+');
+if ((length(name) != (temp2-temp1 + 1))||(length(temp1) == 0))
+  printf("Please enter valid name! \n \n");
+  gender_predictor
+end
+
 parameters_char = {name(end), name(end-1), name(end-2)};
 for i = 1:3
   parameters(i) = double(parameters_char{i})-64;
